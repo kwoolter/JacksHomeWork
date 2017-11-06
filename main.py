721eaa769fe950a446deb14efd215bd0c8a70cc0
kwoolter <keith.woolterton@gmail.com>
2017-11-06 19:33:44 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def main():
+    name = ["Jack", "Rosie", "Jane", "Keith"]
+    verb = ["kicks", "throws", "picks up", "punches"]
+    noun = ["100 tons.", "a dinosuar.", "an Iphone.", "a car."]
+
+    from random import randint
+    def pick(words):
+        num_words = len(words)
+        num_picked = randint(0, num_words - 1)
+        word_picked = words[num_picked]
+        return word_picked
+
+    print(pick(name), pick(verb), pick(noun))
+
+    while True:
+        print(pick(name), pick(verb), pick(noun))
