bed88f32fdab3ea33de4c2835aa77106250416bd
Jack Woolterton <jack.woolterton@yahoo.co.uk>
2017-11-06 20:09:46 +0800

    tidied up code layout

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+from random import randint
+
+
 def main():
     name = ["Jack", "Rosie", "Jane", "Keith"]
     verb = ["kicks", "throws", "picks up", "punches"]
-    noun = ["100 tons.", "a dinosuar.", "an Iphone.", "a car."]
-
-    from random import randint
-    def pick(words):
-        num_words = len(words)
-        num_picked = randint(0, num_words - 1)
-        word_picked = words[num_picked]
-        return word_picked
+    noun = ["100 tons.", "a dinosaur.", "an Iphone.", "a car."]
 
     print(pick(name), pick(verb), pick(noun))
 
     while True:
         print(pick(name), pick(verb), pick(noun))
+
+
+def pick(words):
+    num_words = len(words)
+    num_picked = randint(0, num_words - 1)
+    word_picked = words[num_picked]
+    return word_picked
